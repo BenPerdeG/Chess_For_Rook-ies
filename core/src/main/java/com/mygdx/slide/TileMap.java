7fbe00f0e46072ab4edffac7c843fe0051375af4
dam2a <bperez@elpuig.xeill.net>
2025-04-11 16:56:38 +0200

    aaaaaaaaaaaaaaaaaaaaaaaaaa

--- core/src/main/java/com/mygdx/slide/TileMap.java
+++ core/src/main/java/com/mygdx/slide/TileMap.java
@@ -1,4 +1,42 @@
 package com.mygdx.slide;
 
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.mygdx.slide.jsonloaders.LevelLayout;
+
 public class TileMap {
+    public static final int TILE_SIZE = 64;
+    int width;
+    int height;
+    byte tiles[][];
+    AssetManager manager;
+    SpriteBatch batch;
+
+    public TileMap(AssetManager manager, SpriteBatch batch)
+    {
+        this.manager = manager;
+        this.batch = batch;
+    }
+
+
+    void loadFromLevel(LevelLayout l)
+    {
+        // Load from json file
+        width = l.getMapWidth();
+        height = l.getMapHeight();
+
+        tiles = new byte[height][];
+
+        for(int i = 0; i < height; i++)
+        {
+            tiles[i] = new byte[width];
+        }
+
+        for(int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                tiles[i][j] = l.getTileMap()[i][j];
+            }
+        }
+    }
+
 }
--- core/src/main/java/com/mygdx/slide/jsonloaders/LevelLayout.java
+++ core/src/main/java/com/mygdx/slide/jsonloaders/LevelLayout.java
@@ -1,4 +1,27 @@
 package com.mygdx.slide.jsonloaders;
 
 public class LevelLayout {
+    private int mapWidth;
+    private int mapHeight;
+
+    private byte tileMap[][];
+
+
+    public LevelLayout() {
+    }
+    public LevelLayout(byte[][] tileMap) {
+        this.tileMap = tileMap;
+    }
+
+    public byte[][] getTileMap() {
+        return tileMap;
+    }
+
+    public int getMapWidth() {
+        return mapWidth;
+    }
+
+    public int getMapHeight() {
+        return mapHeight;
+    }
 }
